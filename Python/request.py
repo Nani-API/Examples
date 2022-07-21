87c6fa2e0652a8be588372071b1bf769f4199205
Error44 <hello@error44.tech>
2022-07-21 21:47:55 +0200

    Create request.py

---
+++ Python/request.py
@@ -0,0 +1,21 @@
+# imports
+import aiohttp
+import asyncio
+
+# API token
+token = "token"
+
+# request
+async def nani(main_endpoint, sub_endpoint, response_type: 'json'):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://nani.red/api/{main_endpoint}/{sub_endpoint}/token={token}&type={response_type}/') as r:
+            js = await r.json()
+            return str(js["response"])
+
+# print async environment
+async def main():
+    print(await nani("gif", "cuddle"))
+
+# create async environment
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
