26aeb639bf1e1917d9f8404105059985d3d3a853
Error44 <hello@error44.tech>
2022-11-12 21:16:09 +0100

    Update request-standard.py

--- Python/request-standard.py
+++ Python/request-standard.py
@@ -3,15 +3,15 @@ import aiohttp
 import asyncio
 
 # request
-async def nani(main_endpoint, sub_endpoint, response_type: 'json'):
+async def siesta(main_endpoint, sub_endpoint, response_type: 'json'):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://nani.red/api/{main_endpoint}/{sub_endpoint}/&type={response_type}/') as r:
+        async with session.get(f'https://siesta.red/api/{main_endpoint}/{sub_endpoint}?type={response_type}') as r:
             js = await r.json()
             return str(js["response"])
 
 # print async environment
 async def main():
-    print(await nani("gif", "cuddle"))
+    print(await siesta("gif", "cuddle"))
 
 # create async environment
 loop = asyncio.get_event_loop()
